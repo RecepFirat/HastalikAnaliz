dd79dc16fab66470256de0bb92c5def734954e3f
recep tayyip fırat <rtf3444@gmail.com>
2018-06-14 20:14:48 +0300

    asd

---
+++ hastalikAnaliz/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hastalikAnaliz
+{
+    public partial class Ana_Main : Form
+    {
+        public Ana_Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ hastalikAnaliz/Islemler/Hastalik_Belirti.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hastalikAnaliz.Islemler
+{
+    public enum Hastalik_Belirti
+    {  /*nezle**/
+        istahKayib,
+        nefeshizli,
+        DusukAtes,
+        agizKuru,
+        GucsuzGoruntu,
+        burunTikanik,
+        hapsirma,
+
+        /*****/
+        /*Griportaknezle*/
+        ishal,
+        /*******/
+        /*Grip*/
+        sogukter_vetitreme,
+        eklemveuzuvagrima,
+        bitkinlik,
+        bulanti,
+        kusma,
+
+        /***lejenellozOrtakGrip***/
+        Basagrima,
+        yorgunluk,
+        yuksekAtes,
+        /*****/
+        /***lejenellozOrtak***/
+        kaslardaagri,
+        huzursuluk,
+        dalginlik,
+        bilincBulanik,
+
+
+
+
+        /*Ülser**/
+        midedeYanma,
+        Bulanti,
+        KusmaİleGelenRahatlama,
+        istahsizlik,
+        kilokaybi,
+        siskinlikVegaz,
+        SikAcikma,
+        yemekYediktenSonraMideAgrisi,
+        Hazimsizlik,
+
+        /******/
+        
+        /**********/
+
+        /***/
+    }
+}
